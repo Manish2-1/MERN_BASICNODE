449a17d17bb85ecab63d46cc1bbbe58b39bd2c52
Manish2-1 <man123kc@gmail.com>
2025-09-03 01:02:00 +0545

    change cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-    origin: 'https://mernfrontend-chi.vercel.app',
+    origin: "http://localhost:5173", 'https://mernfrontend-chi.vercel.app',
 }))
 
 app.use(express.json()); // for parsing application/json
